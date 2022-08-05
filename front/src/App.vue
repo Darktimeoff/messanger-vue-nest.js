b38a7ca12017dfb331931a707fad0fa36298fbe8
Yevhenii Korolikhin <yevhenii@devforth.io>
2022-08-05 15:51:16 +0300

    add store

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="wrapper">
     <router-view />
   </div>
 </template>
--- front/src/main.ts
+++ front/src/main.ts
@@ -2,12 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '@/assets/styles/index.scss';
+import { store } from './store';
 // import '@/assets/icons_flat/rocketpot-icons.css';
 
 export const app = createApp(App)
 
 
 app.use(router)
+app.use(store)
 
-// Mounting the app
+
 app.mount('#app')
