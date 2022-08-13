f9e6a56fa44584fbc697a752844d709b051db024
Yevhenii Korolikhin <yevhenii@devforth.io>
2022-08-13 17:54:13 +0300

    add hosting on firebase

--- front/src/pages/index.vue
+++ front/src/pages/index.vue
@@ -1,24 +1,23 @@
 <template>
-    <h1>
-      <Message 
-        avatar="https://avatars.githubusercontent.com/u/60776033?v=4"
-        date="2022-08-13T13:08:04.600Z"
-        :user="{fullname:'Yevhenii Korolikhin'}"
-        isMe
-        isReaded
-      >
-        <MesssageText text="Hello" />
-      </Message>
-      <Message 
-        avatar="https://avatars.githubusercontent.com/u/1838656?v=4"
-        date="2022-08-13T13:26:00.084Z"
-        :user="{fullname:'Ivan Borshov'}"
-        :isMe="false"
-        isReaded
-      >
-        <MesssageText text="Hi" />
-      </Message>
-    </h1>
+    <h1>Friends messanger</h1>
+    <Message 
+      avatar="https://avatars.githubusercontent.com/u/60776033?v=4"
+      date="2022-08-13T13:08:04.600Z"
+      :user="{fullname:'Yevhenii Korolikhin'}"
+      isMe
+      isReaded
+    >
+      <MesssageText text="Hello" />
+    </Message>
+    <Message 
+      avatar="https://avatars.githubusercontent.com/u/1838656?v=4"
+      date="2022-08-13T13:26:00.084Z"
+      :user="{fullname:'Ivan Borshov'}"
+      :isMe="false"
+      isReaded
+    >
+      <MesssageText text="Hi" />
+    </Message>
 </template>
 
 <route lang="yaml">
