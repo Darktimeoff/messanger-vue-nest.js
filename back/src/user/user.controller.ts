47cd58cff1c10d4d6481bd26139398027dfd8f0c
Yevhenii Korolikhin <yevhenii@devforth.io>
2022-08-31 16:09:20 +0300

    create basic controller

--- back/src/user/user.controller.ts
+++ back/src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Post('/') 
+    async create() {
+        const user = await this.userService.create();
+    
+        return user;
+    }
+}
--- back/src/user/user.service.ts
+++ back/src/user/user.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { UserDocument } from './user.model';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectModel('User')
+        private readonly userModel: Model<UserDocument>,
+    ) {
+
+    }
+
+    async create() {
+        return this.userModel.create({
+            fullname: "yevhenii",
+            email: 'yevhenii+10@devforth.io'
+        })
+    }
+}
