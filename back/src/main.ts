1758ea2ecb3b074db1eb42b9ea6f42b46041f0fa
Yevhenii Korolikhin <yevhenii@devforth.io>
2022-08-31 17:14:09 +0300

    add swagger

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const prefix = 'api/v1';
+
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix(prefix);
+
+  const config = new DocumentBuilder()
+  .setTitle('IFriends')
+  .setDescription('The cats IFriends description')
+  .setVersion('1.0')
+  .addTag('ifriends')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(prefix, app, document);
+
   await app.listen(3001);
 }
 
