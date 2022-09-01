dca38fe6677a49aafdf2233873097cc0b8e42575
Yevhenii Korolikhin <yevhenii@devforth.io>
2022-09-01 16:16:07 +0300

    finish schema for dialog and message

--- back/src/dialog/entities/dialog.entity.ts
+++ back/src/dialog/entities/dialog.entity.ts
@@ -1,14 +1,24 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
 import { Document, Types } from 'mongoose';
+import { Message } from 'src/message/entities/message.entity';
 import { User } from 'src/user/entities/user.entity';
 
 export type DialogDocument = Dialog & Document;
 
-
 @Schema({ timestamps: true, _id: true, id: true})
 export class Dialog {
+   @Prop({default: true})
+   isDialog: boolean;
+
+   @Prop({type: Types.ObjectId, ref: "Messsage"})
+   lastMessage: Message[]
+
    @Prop({type: [{type: Types.ObjectId, ref: 'User'}]}) 
-   user: User[]
+   members: User[]
+
+   @Prop({type: [{type: Types.ObjectId, ref: 'Message'}]}) 
+   messsage: Message[]
 }
 
 export const DialogSchema = SchemaFactory.createForClass(Dialog);
--- back/src/message/entities/message.entity.ts
+++ back/src/message/entities/message.entity.ts
@@ -1,4 +1,30 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose"
+import { ApiHideProperty, ApiProperty } from "@nestjs/swagger";
+import { Types } from "mongoose";
+import { Dialog } from "src/dialog/entities/dialog.entity";
+import { User } from "src/user/entities/user.entity";
 
+export type MessageDocument = Message & Document
+
+@Schema({_id: true, id: true, timestamps: true})
 export class Message {
+    @Prop()
+    text: string;
+    
+    @Prop()
+    isRead: boolean;
 
+    @Prop({type: [String]})
+    attachments: string[]
+
+    @Prop()
+    audio: string;
+
+    @Prop({type: Types.ObjectId, ref: 'User'})
+    author: User
+
+    @Prop({type: Types.ObjectId, ref: 'Dialog'})
+    dialog: Dialog
 }
+
+export const MessageSchema = SchemaFactory.createForClass(Message);
